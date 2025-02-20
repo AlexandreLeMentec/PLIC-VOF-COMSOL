854bef3e428b069134a38ae4eb367b791f5b6913
Alexandre Le Mentec <alexandre.lementec@laas.fr>
2025-02-20 16:40:39 +0100

    version that should work on COMSOL, fingers crossed

--- plic_adapted.c
+++ plic_adapted.c
@@ -45,22 +45,22 @@ EXPORT int eval(const char *func, int nArgs, const double **inReal,
             double x = inReal[5][i];
             double z = inReal[6][i];
 
-            // Calcul variables
-            double nuo = x/ dx;
-            double mx = fabs(nx)*dx/(fabs(nz)*dz + fabs(nx)*dx);
-            double mz = fabs(nz)*dz/(fabs(nz)*dz + fabs(nx)*dx);
+            float nuo = x/ dx;
+            float mx = fabs(nx)*dx/(fabs(nz)*dz + fabs(nx)*dx);
+            float mz = fabs(nz)*dz/(fabs(nz)*dz + fabs(nx)*dx);
 
             //
-            double nx_cond = 0.5 + copysign(0.5,nx);                      // condition nx > = 0   
-            double mxz_cond = 0.5 + copysign(0.5,(mz-mx));                // condition mx < = mz
-            double phi_tr = mxz_cond*nx_cond*(mx/(2.*mz)*(nuo+1./3.)/(nuo+1./2.)) + (1.-mxz_cond)*nx_cond*(mz/(2.*mx)*(nuo+mz/(3.*mx))/(nuo+1./2.)) + (1.-nx_cond)*mxz_cond*(mx/(2.*mz)*(nuo+2./3.)/(nuo+1./2)) + (1-nx_cond)*(1-mxz_cond)*(mz/(2*mx)*(nuo+1.-mz/(3*mx))/(nuo+1/2));
-            double phi_crit = nx_cond*(2.*mx/(3.*mz)*(powf(nuo,3.))/(nuo+1./2.)) + (1.-nx_cond)*(2.*mx/(3.*mz)*(powf((nuo+1.),3.))/(nuo+1./2.)) ;
+            float nx_cond = 0.5 + copysign(0.5,nx);                      // condition nx > = 0   
+            float mxz_cond = 0.5 + copysign(0.5,(mz-mx));                // condition mx < = mz
+            float phi_tr = mxz_cond*nx_cond*(mx/(2.*mz)*(nuo+1./3.)/(nuo+1./2.)) + (1.-mxz_cond)*nx_cond*(mz/(2.*mx)*(nuo+mz/(3.*mx))/(nuo+1./2.)) + (1.-nx_cond)*mxz_cond*(mx/(2.*mz)*(nuo+2./3.)/(nuo+1./2)) + (1-nx_cond)*(1-mxz_cond)*(mz/(2*mx)*(nuo+1.-mz/(3*mx))/(nuo+1/2));
+            float phi_crit = nx_cond*(2.*mx/(3.*mz)*(powf(nuo,3.))/(nuo+1./2.)) + (1.-nx_cond)*(2.*mx/(3.*mz)*(powf((nuo+1.),3.))/(nuo+1./2.)) ;
             //
 
-            double M = 3*powf(mx,2.)*mz*(nuo+0.5);
-            double phio = asin(1. - 2.*C/phi_crit);
-            double alpha = 0.        ;
-            // double phimax 
+            float M = 3*powf(mx,2.)*mz*(nuo+0.5);
+            float phio = asin(1. - 2.*C/phi_crit);
+            float alpha = 0.;
+            
+            // float phimax 
 
             // #################################### CONDITIONNER ALPHA ##################################
             
@@ -68,14 +68,15 @@ EXPORT int eval(const char *func, int nArgs, const double **inReal,
             {
                 if (C < phi_crit || nx < 0.)                    // equation (1 et 6) et (4 et 9)
                 {
-                double alpha_a = (-1 + 2*cos(phio/3.0 + acos(-1)/6.0 )) ;
-                double alpha_b = (-1 + 2*sin(phio/3.0)) ;
-                double alpha = powf(M*phi_crit/2,1/3) * (nx_cond*alpha_a + (1. - nx_cond)*alpha_b)  ;   
+                float alpha_a = (-1 + 2*cos(phio/3 + acos(-1.0)/6.0 )) ;
+                float alpha_b = (-1 + 2*sin(phio/3)) ;
+                alpha = copysign(1.0,nx)*powf(M*phi_crit*0.5,1.0/3.0) * (nx_cond*alpha_a + (1. - nx_cond)*alpha_b)  ;   
+
                 }
                 else                 // equation 2 et 7
                 {
-                double alpha_c = powf(2.*C - phi_crit + 2.*sqrt(C*(C - phi_crit)),1/3) + powf(2.*C - phi_crit - 2*sqrt(C*C - phi_crit),1./3.);
-                double alpha = ( alpha_c + copysign(1.0,(mz-mx))*pow(phi_crit,1/3))*pow(M/2,1/3) ;
+                float alpha_c = powf(2.*C - phi_crit + 2.*sqrt(C*(C - phi_crit)),1.0/3.0) + powf(2.*C - phi_crit - 2*sqrt(C*C - phi_crit),1.0/3.0);
+                alpha = ( alpha_c + copysign(1.0,(mz-mx))*powf(phi_crit,1.0/3.0))*powf(M/2.0,1.0/3.0) ;
                 }
             }
             else
@@ -83,19 +84,19 @@ EXPORT int eval(const char *func, int nArgs, const double **inReal,
                 if((mz-mx)>=0.)
                 {
                     // equation 3 et 5
-                    double alpha = mz * C + mx/2. * (nuo + 1/3 + nx_cond*1.0/3.0)/(nuo+1/2) ; // Ajout d'une condition sur le 1/3 -> 2/3
+                    alpha = mz * C + mx/2.0 * (nuo + 1/3 + nx_cond*1.0/3.0)/(nuo+0.5) ; // Ajout d'une condition sur le 1/3 -> 2/3
                 }
                 else
                 {
                     if(nx<0.)
                     {
                         // equation 10
-                        double alpha = 1/2 * (mz - 2*mx*(nuo+1) - sqrt(4*powf(mx,2)*pow(nuo+1,2)- 8 * powf(mx,2)*(nuo+1/2)*C - 1/3*powf(mz,2)));
+                        alpha = 1/2 * (mz - 2*mx*(nuo+1) - sqrt(4*powf(mx,2.0)*powf(nuo+1,2.0)- 8 * powf(mx,2.0)*(nuo+0.5)*C - 1.0/3.0*powf(mz,2.0)));
                     }
                     else
                     {
                         // equation 8 
-                        double alpha = 1/2 * (mz - 2*mx*nuo + sqrt(4*powf(mx,2)*powf(nuo,2) + 8 * powf(mx,2)*(nuo+1/2)*C - 1/3*powf(mz,2)));
+                        alpha = 0.5 * (mz - 2*mx*nuo + sqrt(4*powf(mx,2.0)*powf(nuo,2.0) + 8 * powf(mx,2.0)*(nuo+0.5)*C - 1.0/3.0*powf(mz,2.0)));
                     }
                 }
             }
@@ -109,21 +110,27 @@ EXPORT int eval(const char *func, int nArgs, const double **inReal,
                 };
             tuple results[2];
             int count = 0;
-            for (int i = 0; i < 4; i++) {
-                int mask = (candidates[i].x >= 0.0f && candidates[i].x <= 1.0f) &
-                        (candidates[i].z >= 0.0f && candidates[i].z <= 1.0f);
-                results[count] = candidates[i];  
+            for (int j = 0; j < 4; j++) {
+                int mask = (candidates[j].x >= 0.0f && candidates[j].x <= 1.0f) &
+                        (candidates[j].z >= 0.0f && candidates[j].z <= 1.0f);
+                results[count] = candidates[j];  
                 count += mask;
                 if (count == 2) break;
             }
             // results[0] => {sigx1, sigz1}, results[0].x = sigx1
             
             tuple results_coord[2];
-            for (int i = 0; i < 2; i++) {
-                results_coord[i].x = dx * results[i].z + x     ;
-                results_coord[i].z = dz * (1- results[i].x)+z*dz  ;
-            }  
-            outReal[i] = sqrt((results_coord[1].x - results_coord[0].x)*(results_coord[1].x - results_coord[0].x) + (results_coord[1].z - results_coord[0].z)*(results_coord[1].z - results_coord[0].z));
+            if (count == 2){
+                for (int j = 0; j < 2; j++) {
+                    results_coord[j].x = dx * results[j].x + x     ;
+                    results_coord[j].z = dz * (1- results[j].z)+z*dz  ;
+                }  
+                float Si = sqrt((results_coord[1].x - results_coord[0].x)*(results_coord[1].x - results_coord[0].x) + (results_coord[1].z - results_coord[0].z)*(results_coord[1].z - results_coord[0].z));
+                outReal[i] = Si;
+            }
+            else {
+                outReal[i] = 0.0;
+            }
         }
         return 1;
     }
